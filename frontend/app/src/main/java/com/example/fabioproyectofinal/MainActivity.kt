940c68f54053d5f0ca2379959b97778f2d7eb736
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2025-04-19 19:41:47 +0200

    Añadimos la barra de búsqueda superior para que se vea en todas las pantallas y quitamos imports innecesarios

--- frontend/app/src/main/java/com/example/fabioproyectofinal/MainActivity.kt
+++ frontend/app/src/main/java/com/example/fabioproyectofinal/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.fabioproyectofinal.navigation.AppNavigation
 import com.example.fabioproyectofinal.ui.theme.FabioProyectoFInalTheme
 import com.example.fabioproyectofinal.view.BottomNavigationBar
+import com.example.fabioproyectofinal.view.TopBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +21,7 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             FabioProyectoFInalTheme {
                 Scaffold(
+                    topBar = { TopBar()},
                     bottomBar = { BottomNavigationBar() }
                 ) { innerPadding ->
                     AppNavigation(navController = navController, modifier = Modifier.padding(innerPadding))
--- frontend/app/src/main/java/com/example/fabioproyectofinal/view/MainScreenApp.kt
+++ frontend/app/src/main/java/com/example/fabioproyectofinal/view/MainScreenApp.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.fabioproyectofinal.model.data.Clinic
 import com.example.fabioproyectofinal.model.data.clinics
