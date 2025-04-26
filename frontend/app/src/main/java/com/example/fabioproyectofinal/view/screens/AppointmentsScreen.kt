f858824cbbdebe8020a1ffc828ac8ddc960bd8c7
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2025-04-27 01:55:14 +0200

    Creamos diseño de historial y confirmadas

--- frontend/app/src/main/java/com/example/fabioproyectofinal/view/screens/AppointmentsScreen.kt
+++ frontend/app/src/main/java/com/example/fabioproyectofinal/view/screens/AppointmentsScreen.kt
@@ -2,31 +2,20 @@ package com.example.fabioproyectofinal.view.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.fabioproyectofinal.model.data.Clinic
-import com.example.fabioproyectofinal.model.data.clinics
-import com.example.fabioproyectofinal.view.components.ClinicaCard
 import com.example.fabioproyectofinal.view.components.TopBar
 
 @Composable
 fun AppointmentsScreen(navController: NavHostController) {
-    var searchText by remember { mutableStateOf("") }
-    val clinicasFiltradas = clinics.filter {
-        it.name.contains(searchText, ignoreCase = true) ||
-                it.address.contains(searchText, ignoreCase = true)
-    }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,6 +26,7 @@ fun AppointmentsScreen(navController: NavHostController) {
         TopBar("Fabio González Waschkowitz", navController = navController) { /* Acción */ }
         // "Buscador"
         Row {
+            // "Citas"
             Text(
                 text = "Citas",
                 color = Color(0xFFB2C2A4),
@@ -44,20 +34,48 @@ fun AppointmentsScreen(navController: NavHostController) {
                 modifier = Modifier
                     .padding(start = 16.dp, bottom = 16.dp)
             )
-            Text(
-                text = "Historial",
-                color = Color(0xFFB2C2A4),
-                fontSize = 40.sp,
+            // "Historia"
+            Card(
                 modifier = Modifier
-                    .padding(start = 16.dp, bottom = 16.dp)
-            )
+                    .size(width = 180.dp, height = 45.dp),
+                shape = RoundedCornerShape(10.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    Text(
+                        text = "Historial:",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color(0xFFB2C2A4),
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+            }
         }
-        Text(
-            text = "Confirmadas",
-            color = Color(0xFFB2C2A4),
-            fontSize = 40.sp,
+        // "Confirmadas"
+        Card(
             modifier = Modifier
-                .padding(start = 16.dp, bottom = 16.dp)
-        )
+                .size(width = 180.dp, height = 45.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = "Confirmadas:",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color(0xFFB2C2A4),
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+        }
     }
 }
