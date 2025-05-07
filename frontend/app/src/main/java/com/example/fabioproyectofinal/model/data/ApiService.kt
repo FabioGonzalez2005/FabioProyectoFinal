f8e5854b89639c3c9825207b308a1c26bb6298f7
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2025-05-07 21:27:06 +0200

    Creamos ApiServer y ApiService

--- frontend/app/src/main/java/com/example/fabioproyectofinal/model/data/ApiService.kt
+++ frontend/app/src/main/java/com/example/fabioproyectofinal/model/data/ApiService.kt
@@ -1,2 +1,9 @@
-package com.example.fabioproyectofinal.model.data
+package com.example.fabioproyectofinal.model
 
+import com.example.fabioproyectofinal.model.data.model.Clinic
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("/clinicas")
+    suspend fun getClinics(): List<Clinic>
+}
