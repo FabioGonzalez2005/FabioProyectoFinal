9df6d2efe2ed2474c28f98268fd25451029486df
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2025-05-25 19:08:32 +0200

    Quitamos imports innecesarios

--- frontend/app/src/main/java/com/example/fabioproyectofinal/view/components/EditRecordDialog.kt
+++ frontend/app/src/main/java/com/example/fabioproyectofinal/view/components/EditRecordDialog.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,16 +15,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.fabioproyectofinal.model.session.SessionManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import android.widget.Toast
 import androidx.compose.ui.platform.LocalContext
-import com.example.fabioproyectofinal.model.ApiServer
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import com.example.fabioproyectofinal.R
